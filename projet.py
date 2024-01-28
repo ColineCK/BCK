df8ff7e14db60f4613c6508c4380d6fc904fac36
ColineKettaneh <144048922+ColineCK@users.noreply.github.com>
2024-01-28 14:47:34 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -304,7 +304,7 @@ def graph_nuage_mots(variable):
                 stopwords = mots_exclus,
                 max_words=30,
                 colormap="Reds",
-		font_path='arial.ttf',
+		font_path='DejaVuSans.ttf',
                 min_font_size = 10).generate(comment_words)
     
  
